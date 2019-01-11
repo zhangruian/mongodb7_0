mongodb/mongo

SERVER-38527 Merge supportsRecoverToStableTimestamp into supportsRecoveryTimestamp

src/mongo/db/repl/bgsync.cpp
@@ -629,7 +629,7 @@ void BackgroundSync::_runRollback(OperationContext* opCtx,
     storageInterface->waitForAllEarlierOplogWritesToBeVisible(opCtx);
 
     auto storageEngine = opCtx->getServiceContext()->getStorageEngine();
-    if (!forceRollbackViaRefetch.load() && storageEngine->supportsRecoverToStableTimestamp()) {
+    if (!forceRollbackViaRefetch.load() && storageEngine->supportsRecoveryTimestamp()) {
         log() << "Rollback using 'recoverToStableTimestamp' method.";
         _runRollbackViaRecoverToCheckpoint(
             opCtx, source, &localOplog, storageInterface, getConnection);

src/mongo/db/storage/kv/kv_storage_engine.cpp
@@ -705,10 +705,6 @@ void KVStorageEngine::setCachePressureForTest(int pressure) {
     return _engine->setCachePressureForTest(pressure);
 }
 
-bool KVStorageEngine::supportsRecoverToStableTimestamp() const {
-    return _engine->supportsRecoverToStableTimestamp();
-}
-
 bool KVStorageEngine::supportsRecoveryTimestamp() const {
     return _engine->supportsRecoveryTimestamp();
 }
@@ -756,7 +752,7 @@ bool KVStorageEngine::supportsReadConcernMajority() const {
 }
 
 bool KVStorageEngine::supportsPendingDrops() const {
-    return enableKVPendingDrops && supportsRecoverToStableTimestamp();
+    return enableKVPendingDrops && supportsRecoveryTimestamp();
 }
 
 void KVStorageEngine::clearDropPendingState() {

zhangruian/mongodb7_0

src/mongo/db/repl/bgsync.cpp
@@ -629,7 +629,7 @@ void BackgroundSync::_runRollback(OperationContext* opCtx,
     storageInterface->waitForAllEarlierOplogWritesToBeVisible(opCtx);
 
     auto storageEngine = opCtx->getServiceContext()->getStorageEngine();
-    if (!forceRollbackViaRefetch.load() && storageEngine->supportsRecoverToStableTimestamp()) {
+    if (!forceRollbackViaRefetch.load() && storageEngine->supportsReadConcernMajority()) {
         log() << "Rollback using 'recoverToStableTimestamp' method.";
         _runRollbackViaRecoverToCheckpoint(
             opCtx, source, &localOplog, storageInterface, getConnection);

src/mongo/db/storage/kv/kv_storage_engine.cpp
@@ -705,10 +705,6 @@ void KVStorageEngine::setCachePressureForTest(int pressure) {
     return _engine->setCachePressureForTest(pressure);
 }
 
-bool KVStorageEngine::supportsRecoverToStableTimestamp() const {
-    return _engine->supportsRecoverToStableTimestamp();
-}
-
 bool KVStorageEngine::supportsRecoveryTimestamp() const {
     return _engine->supportsRecoveryTimestamp();
 }
@@ -756,7 +752,7 @@ bool KVStorageEngine::supportsReadConcernMajority() const {
 }
 
 bool KVStorageEngine::supportsPendingDrops() const {
-    return enableKVPendingDrops && supportsRecoverToStableTimestamp();
+    return enableKVPendingDrops && supportsReadConcernMajority();
 }
 
 void KVStorageEngine::clearDropPendingState() {