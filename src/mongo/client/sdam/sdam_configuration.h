lamontnelson/mongo

SERVER-47606: make SDAM timeout values into server parameters

src/mongo/client/sdam/sdam_configuration.h
@@ -30,6 +30,8 @@
 
 #include "mongo/client/sdam/sdam_datatypes.h"
 
+#include "mongo/client/sdam/sdam_configuration_parameters_gen.h"
+
 namespace mongo::sdam {
 class SdamConfiguration {
 public:
@@ -54,47 +56,40 @@ public:
      * If setName is not null, only TopologyType ReplicaSetNoPrimary and Single, are
      * allowed.
      */
-    explicit SdamConfiguration(boost::optional<std::vector<ServerAddress>> seedList,
-                               TopologyType initialType = TopologyType::kUnknown,
-                               Milliseconds heartBeatFrequencyMs = kDefaultHeartbeatFrequencyMs,
-                               boost::optional<std::string> setName = boost::none);
+    explicit SdamConfiguration(
+        boost::optional<std::vector<ServerAddress>> seedList,
+        TopologyType initialType = TopologyType::kUnknown,
+        Milliseconds heartBeatFrequencyMs = Milliseconds(sdamHeartBeatFrequencyMs),
+        Milliseconds connectTimeoutMs = Milliseconds(sdamConnectTimeoutMs),
+        Milliseconds localThreshholdMs = Milliseconds(sdamLocalThreshholdMs),
+        Milliseconds serverSelectionTimeoutMs = Milliseconds(sdamServerSelectionTimeoutMs),
+        boost::optional<std::string> setName = boost::none);
 
     const boost::optional<std::vector<ServerAddress>>& getSeedList() const;
     TopologyType getInitialType() const;
-    Milliseconds getHeartBeatFrequency() const;
     const boost::optional<std::string>& getSetName() const;
 
-    static constexpr Milliseconds kDefaultHeartbeatFrequencyMs = Seconds(10);
-    static constexpr Milliseconds kMinHeartbeatFrequencyMS = Milliseconds(500);
-    static constexpr Milliseconds kDefaultConnectTimeoutMS = Milliseconds(10000);
+    Milliseconds getHeartBeatFrequency() const;
+    Milliseconds getConnectionTimeout() const;
+    Milliseconds getLocalThreshold() const;
+    Milliseconds getServerSelectionTimeout() const;
 
-private:
-    boost::optional<std::vector<ServerAddress>> _seedList;
-    TopologyType _initialType;
-    Milliseconds _heartBeatFrequencyMs;
-    boost::optional<std::string> _setName;
-};
-
-class ServerSelectionConfiguration {
-public:
-    explicit ServerSelectionConfiguration(const Milliseconds localThresholdMs,
-                                          const Milliseconds serverSelectionTimeoutMs);
-
-    Milliseconds getLocalThresholdMs() const;
-    Milliseconds getServerSelectionTimeoutMs() const;
-    Milliseconds getHeartBeatFrequencyMs() const;
-
-    static constexpr Milliseconds kDefaultLocalThresholdMS = Milliseconds(15);
-    static constexpr Milliseconds kDefaultServerSelectionTimeoutMs = Milliseconds(30000);
-
-    static ServerSelectionConfiguration defaultConfiguration() {
-        return ServerSelectionConfiguration{kDefaultLocalThresholdMS,
-                                            kDefaultServerSelectionTimeoutMs};
+    const BSONObj& toBson() const {
+        return _bsonDoc;
     }
 
+    static constexpr Milliseconds kMinHeartbeatFrequencyMS = Milliseconds(500);
+
 private:
-    Milliseconds _localThresholdMs = kDefaultLocalThresholdMS;
-    Milliseconds _serverSelectionTimeoutMs = kDefaultServerSelectionTimeoutMs;
-    Milliseconds _heartBeatFrequencyMs = SdamConfiguration::kDefaultHeartbeatFrequencyMs;
+    BSONObj _toBson() const;
+
+    boost::optional<std::vector<ServerAddress>> _seedList;
+    TopologyType _initialType;
+    Milliseconds _heartbeatFrequency;
+    Milliseconds _connectionTimeout;
+    Milliseconds _localThreshold;
+    Milliseconds _serverSelectionTimeout;
+    boost::optional<std::string> _setName;
+    BSONObj _bsonDoc;
 };
 }  // namespace mongo::sdam

src/mongo/client/sdam/topology_description_test.cpp
@@ -56,6 +56,13 @@ class TopologyDescriptionTestFixture : public SdamTestFixture {
 protected:
     void assertDefaultConfig(const TopologyDescription& topologyDescription);
 
+    SdamConfiguration makeSdamConfig(std::vector<ServerAddress> servers,
+                                     TopologyType topologyType,
+                                     boost::optional<std::string> setName) {
+        return SdamConfiguration(
+            servers, topologyType, kNotUsedMs, kNotUsedMs, kNotUsedMs, kNotUsedMs, setName);
+    }
+
     static inline const auto kSetName = std::string("mySetName");
 
     static inline const std::vector<ServerAddress> kOneServer{"foo:1234"};
@@ -67,6 +74,8 @@ protected:
     static inline const auto kDefaultConfig = SdamConfiguration();
     static inline const auto kSingleSeedConfig =
         SdamConfiguration(kOneServer, TopologyType::kSingle);
+
+    static inline const auto kNotUsedMs = Milliseconds{1000};
 };
 
 void TopologyDescriptionTestFixture::assertDefaultConfig(
@@ -123,25 +132,22 @@ TEST_F(TopologyDescriptionTestFixture, DoesNotAllowMultipleSeedsWithSingle) {
 
 TEST_F(TopologyDescriptionTestFixture, ShouldSetTheReplicaSetName) {
     auto expectedSetName = kSetName;
-    auto config = SdamConfiguration(
-        kOneServer, TopologyType::kReplicaSetNoPrimary, mongo::Seconds(10), expectedSetName);
-    TopologyDescription topologyDescription(config);
+    TopologyDescription topologyDescription(
+        makeSdamConfig(kOneServer, TopologyType::kReplicaSetNoPrimary, expectedSetName));
     ASSERT_EQUALS(expectedSetName, *topologyDescription.getSetName());
 }
 
 TEST_F(TopologyDescriptionTestFixture, ShouldNotAllowSettingTheReplicaSetNameWithWrongType) {
-    ASSERT_THROWS_CODE(TopologyDescription(SdamConfiguration(
-                           kOneServer, TopologyType::kUnknown, mongo::Seconds(10), kSetName)),
-                       DBException,
-                       ErrorCodes::InvalidTopologyType);
+    ASSERT_THROWS_CODE(
+        TopologyDescription(makeSdamConfig(kOneServer, TopologyType::kUnknown, kSetName)),
+        DBException,
+        ErrorCodes::InvalidTopologyType);
 }
 
 TEST_F(TopologyDescriptionTestFixture, ShouldNotAllowTopologyTypeRSNoPrimaryWithoutSetName) {
-    ASSERT_THROWS_CODE(
-        SdamConfiguration(
-            kOneServer, TopologyType::kReplicaSetNoPrimary, mongo::Seconds(10), boost::none),
-        DBException,
-        ErrorCodes::TopologySetNameRequired);
+    ASSERT_THROWS_CODE(makeSdamConfig(kOneServer, TopologyType::kReplicaSetNoPrimary, boost::none),
+                       DBException,
+                       ErrorCodes::TopologySetNameRequired);
 }
 
 TEST_F(TopologyDescriptionTestFixture, ShouldOnlyAllowSingleAndRsNoPrimaryWithSetName) {
@@ -159,16 +165,15 @@ TEST_F(TopologyDescriptionTestFixture, ShouldOnlyAllowSingleAndRsNoPrimaryWithSe
               "Check TopologyType {topologyType} with setName value.",
               "Check TopologyType with setName value",
               "topologyType"_attr = topologyType);
-        ASSERT_THROWS_CODE(
-            SdamConfiguration(kOneServer, topologyType, mongo::Seconds(10), kSetName),
-            DBException,
-            ErrorCodes::InvalidTopologyType);
+        ASSERT_THROWS_CODE(makeSdamConfig(kOneServer, topologyType, kSetName),
+                           DBException,
+                           ErrorCodes::InvalidTopologyType);
     }
 }
 
 TEST_F(TopologyDescriptionTestFixture, ShouldDefaultHeartbeatToTenSecs) {
     SdamConfiguration config;
-    ASSERT_EQUALS(SdamConfiguration::kDefaultHeartbeatFrequencyMs, config.getHeartBeatFrequency());
+    ASSERT_EQUALS(Seconds{10}, config.getHeartBeatFrequency());
 }
 
 TEST_F(TopologyDescriptionTestFixture, ShouldAllowSettingTheHeartbeatFrequency) {
@@ -189,7 +194,7 @@ TEST_F(TopologyDescriptionTestFixture, ShouldNotAllowChangingTheHeartbeatFrequen
 TEST_F(TopologyDescriptionTestFixture,
        ShouldSetWireCompatibilityErrorForMinWireVersionWhenMinWireVersionIsGreater) {
     const auto outgoingMaxWireVersion = WireSpec::instance().outgoing.maxWireVersion;
-    const auto config = SdamConfiguration(kOneServer, TopologyType::kUnknown, mongo::Seconds(10));
+    const auto config = SdamConfiguration(kOneServer, TopologyType::kUnknown);
     const auto topologyDescription = std::make_shared<TopologyDescription>(config);
     const auto serverDescriptionMinVersion = ServerDescriptionBuilder()
                                                  .withAddress(kOneServer[0])
@@ -206,7 +211,7 @@ TEST_F(TopologyDescriptionTestFixture,
 TEST_F(TopologyDescriptionTestFixture,
        ShouldSetWireCompatibilityErrorForMinWireVersionWhenMaxWireVersionIsLess) {
     const auto outgoingMinWireVersion = WireSpec::instance().outgoing.minWireVersion;
-    const auto config = SdamConfiguration(kOneServer, TopologyType::kUnknown, mongo::Seconds(10));
+    const auto config = SdamConfiguration(kOneServer, TopologyType::kUnknown);
     const auto topologyDescription = std::make_shared<TopologyDescription>(config);
     const auto serverDescriptionMaxVersion = ServerDescriptionBuilder()
                                                  .withAddress(kOneServer[0])
@@ -222,7 +227,7 @@ TEST_F(TopologyDescriptionTestFixture,
 
 TEST_F(TopologyDescriptionTestFixture, ShouldNotSetWireCompatibilityErrorWhenServerTypeIsUnknown) {
     const auto outgoingMinWireVersion = WireSpec::instance().outgoing.minWireVersion;
-    const auto config = SdamConfiguration(kOneServer, TopologyType::kUnknown, mongo::Seconds(10));
+    const auto config = SdamConfiguration(kOneServer, TopologyType::kUnknown);
     const auto topologyDescription = std::make_shared<TopologyDescription>(config);
     const auto serverDescriptionMaxVersion =
         ServerDescriptionBuilder().withMaxWireVersion(outgoingMinWireVersion - 1).instance();

zhangruian/mongodb7_0

src/mongo/client/sdam/sdam_configuration.h
@@ -28,6 +28,7 @@
  */
 #pragma once
 
+#include "mongo/client/sdam/sdam_configuration_parameters_gen.h"
 #include "mongo/client/sdam/sdam_datatypes.h"
 
 namespace mongo::sdam {
@@ -54,47 +55,61 @@ public:
      * If setName is not null, only TopologyType ReplicaSetNoPrimary and Single, are
      * allowed.
      */
-    explicit SdamConfiguration(boost::optional<std::vector<HostAndPort>> seedList,
-                               TopologyType initialType = TopologyType::kUnknown,
-                               Milliseconds heartBeatFrequencyMs = kDefaultHeartbeatFrequencyMs,
-                               boost::optional<std::string> setName = boost::none);
+    explicit SdamConfiguration(
+        boost::optional<std::vector<HostAndPort>> seedList,
+        TopologyType initialType = TopologyType::kUnknown,
+        Milliseconds heartBeatFrequencyMs = Milliseconds(sdamHeartBeatFrequencyMs),
+        Milliseconds connectTimeoutMs = Milliseconds(sdamConnectTimeoutMs),
+        Milliseconds localThreshholdMs = Milliseconds(sdamLocalThreshholdMs),
+        boost::optional<std::string> setName = boost::none);
 
+    /**
+     * The initial set of servers to monitor in the replica set.
+     */
     const boost::optional<std::vector<HostAndPort>>& getSeedList() const;
+
+    /**
+     * The initial type of the replica set.
+     */
     TopologyType getInitialType() const;
-    Milliseconds getHeartBeatFrequency() const;
+
+    /**
+     * The replica set name.
+     */
     const boost::optional<std::string>& getSetName() const;
 
-    static constexpr Milliseconds kDefaultHeartbeatFrequencyMs = Seconds(10);
-    static constexpr Milliseconds kMinHeartbeatFrequencyMS = Milliseconds(500);
-    static constexpr Milliseconds kDefaultConnectTimeoutMS = Milliseconds(10000);
+    /**
+     * The frequency at which we measure RTT and IsMaster responses.
+     */
+    Milliseconds getHeartBeatFrequency() const;
 
-private:
-    boost::optional<std::vector<HostAndPort>> _seedList;
-    TopologyType _initialType;
-    Milliseconds _heartBeatFrequencyMs;
-    boost::optional<std::string> _setName;
-};
+    /**
+     * How long to wait to obtain a connection.
+     */
+    Milliseconds getConnectionTimeout() const;
 
-class ServerSelectionConfiguration {
-public:
-    explicit ServerSelectionConfiguration(const Milliseconds localThresholdMs,
-                                          const Milliseconds serverSelectionTimeoutMs);
+    /**
+     * The width of the latency window used in server selection.
+     */
+    Milliseconds getLocalThreshold() const;
 
-    Milliseconds getLocalThresholdMs() const;
-    Milliseconds getServerSelectionTimeoutMs() const;
-    Milliseconds getHeartBeatFrequencyMs() const;
-
-    static constexpr Milliseconds kDefaultLocalThresholdMS = Milliseconds(15);
-    static constexpr Milliseconds kDefaultServerSelectionTimeoutMs = Milliseconds(30000);
-
-    static ServerSelectionConfiguration defaultConfiguration() {
-        return ServerSelectionConfiguration{kDefaultLocalThresholdMS,
-                                            kDefaultServerSelectionTimeoutMs};
+    const BSONObj& toBson() const {
+        return _bsonDoc;
     }
 
+    static constexpr Milliseconds kMinHeartbeatFrequency = Milliseconds(500);
+
 private:
-    Milliseconds _localThresholdMs = kDefaultLocalThresholdMS;
-    Milliseconds _serverSelectionTimeoutMs = kDefaultServerSelectionTimeoutMs;
-    Milliseconds _heartBeatFrequencyMs = SdamConfiguration::kDefaultHeartbeatFrequencyMs;
+    BSONObj _toBson() const;
+
+    boost::optional<std::vector<HostAndPort>> _seedList;
+    TopologyType _initialType;
+
+    Milliseconds _heartbeatFrequency;
+    Milliseconds _connectionTimeout;
+    Milliseconds _localThreshold;
+
+    boost::optional<std::string> _setName;
+    BSONObj _bsonDoc;
 };
 }  // namespace mongo::sdam

src/mongo/client/sdam/topology_description_test.cpp
@@ -55,6 +55,13 @@ class TopologyDescriptionTestFixture : public SdamTestFixture {
 protected:
     void assertDefaultConfig(const TopologyDescription& topologyDescription);
 
+    SdamConfiguration makeSdamConfig(std::vector<HostAndPort> servers,
+                                     TopologyType topologyType,
+                                     boost::optional<std::string> setName) {
+        return SdamConfiguration(
+            servers, topologyType, kNotUsedMs, kNotUsedMs, kNotUsedMs, setName);
+    }
+
     static inline const auto kSetName = std::string("mySetName");
 
     static inline const std::vector<HostAndPort> kOneServer{HostAndPort("foo:1234")};
@@ -68,6 +75,8 @@ protected:
     static inline const auto kDefaultConfig = SdamConfiguration();
     static inline const auto kSingleSeedConfig =
         SdamConfiguration(kOneServer, TopologyType::kSingle);
+
+    static inline const auto kNotUsedMs = Milliseconds{1000};
 };
 
 void TopologyDescriptionTestFixture::assertDefaultConfig(
@@ -124,25 +133,22 @@ TEST_F(TopologyDescriptionTestFixture, DoesNotAllowMultipleSeedsWithSingle) {
 
 TEST_F(TopologyDescriptionTestFixture, ShouldSetTheReplicaSetName) {
     auto expectedSetName = kSetName;
-    auto config = SdamConfiguration(
-        kOneServer, TopologyType::kReplicaSetNoPrimary, mongo::Seconds(10), expectedSetName);
-    TopologyDescription topologyDescription(config);
+    TopologyDescription topologyDescription(
+        makeSdamConfig(kOneServer, TopologyType::kReplicaSetNoPrimary, expectedSetName));
     ASSERT_EQUALS(expectedSetName, *topologyDescription.getSetName());
 }
 
 TEST_F(TopologyDescriptionTestFixture, ShouldNotAllowSettingTheReplicaSetNameWithWrongType) {
-    ASSERT_THROWS_CODE(TopologyDescription(SdamConfiguration(
-                           kOneServer, TopologyType::kUnknown, mongo::Seconds(10), kSetName)),
-                       DBException,
-                       ErrorCodes::InvalidTopologyType);
+    ASSERT_THROWS_CODE(
+        TopologyDescription(makeSdamConfig(kOneServer, TopologyType::kUnknown, kSetName)),
+        DBException,
+        ErrorCodes::InvalidTopologyType);
 }
 
 TEST_F(TopologyDescriptionTestFixture, ShouldNotAllowTopologyTypeRSNoPrimaryWithoutSetName) {
-    ASSERT_THROWS_CODE(
-        SdamConfiguration(
-            kOneServer, TopologyType::kReplicaSetNoPrimary, mongo::Seconds(10), boost::none),
-        DBException,
-        ErrorCodes::TopologySetNameRequired);
+    ASSERT_THROWS_CODE(makeSdamConfig(kOneServer, TopologyType::kReplicaSetNoPrimary, boost::none),
+                       DBException,
+                       ErrorCodes::TopologySetNameRequired);
 }
 
 TEST_F(TopologyDescriptionTestFixture, ShouldOnlyAllowSingleAndRsNoPrimaryWithSetName) {
@@ -160,16 +166,15 @@ TEST_F(TopologyDescriptionTestFixture, ShouldOnlyAllowSingleAndRsNoPrimaryWithSe
               "Check TopologyType {topologyType} with setName value.",
               "Check TopologyType with setName value",
               "topologyType"_attr = topologyType);
-        ASSERT_THROWS_CODE(
-            SdamConfiguration(kOneServer, topologyType, mongo::Seconds(10), kSetName),
-            DBException,
-            ErrorCodes::InvalidTopologyType);
+        ASSERT_THROWS_CODE(makeSdamConfig(kOneServer, topologyType, kSetName),
+                           DBException,
+                           ErrorCodes::InvalidTopologyType);
     }
 }
 
 TEST_F(TopologyDescriptionTestFixture, ShouldDefaultHeartbeatToTenSecs) {
     SdamConfiguration config;
-    ASSERT_EQUALS(SdamConfiguration::kDefaultHeartbeatFrequencyMs, config.getHeartBeatFrequency());
+    ASSERT_EQUALS(Seconds{10}, config.getHeartBeatFrequency());
 }
 
 TEST_F(TopologyDescriptionTestFixture, ShouldAllowSettingTheHeartbeatFrequency) {
@@ -180,7 +185,7 @@ TEST_F(TopologyDescriptionTestFixture, ShouldAllowSettingTheHeartbeatFrequency) 
 
 TEST_F(TopologyDescriptionTestFixture, ShouldNotAllowChangingTheHeartbeatFrequencyBelow500Ms) {
     auto belowThresholdFrequency =
-        mongo::Milliseconds(SdamConfiguration::kMinHeartbeatFrequencyMS.count() - 1);
+        mongo::Milliseconds(SdamConfiguration::kMinHeartbeatFrequency.count() - 1);
     ASSERT_THROWS_CODE(
         SdamConfiguration(boost::none, TopologyType::kUnknown, belowThresholdFrequency),
         DBException,
@@ -190,7 +195,7 @@ TEST_F(TopologyDescriptionTestFixture, ShouldNotAllowChangingTheHeartbeatFrequen
 TEST_F(TopologyDescriptionTestFixture,
        ShouldSetWireCompatibilityErrorForMinWireVersionWhenMinWireVersionIsGreater) {
     const auto outgoingMaxWireVersion = WireSpec::instance().outgoing.maxWireVersion;
-    const auto config = SdamConfiguration(kOneServer, TopologyType::kUnknown, mongo::Seconds(10));
+    const auto config = SdamConfiguration(kOneServer, TopologyType::kUnknown);
     const auto topologyDescription = std::make_shared<TopologyDescription>(config);
     const auto serverDescriptionMinVersion = ServerDescriptionBuilder()
                                                  .withAddress(kOneServer[0])
@@ -207,7 +212,7 @@ TEST_F(TopologyDescriptionTestFixture,
 TEST_F(TopologyDescriptionTestFixture,
        ShouldSetWireCompatibilityErrorForMinWireVersionWhenMaxWireVersionIsLess) {
     const auto outgoingMinWireVersion = WireSpec::instance().outgoing.minWireVersion;
-    const auto config = SdamConfiguration(kOneServer, TopologyType::kUnknown, mongo::Seconds(10));
+    const auto config = SdamConfiguration(kOneServer, TopologyType::kUnknown);
     const auto topologyDescription = std::make_shared<TopologyDescription>(config);
     const auto serverDescriptionMaxVersion = ServerDescriptionBuilder()
                                                  .withAddress(kOneServer[0])
@@ -223,7 +228,7 @@ TEST_F(TopologyDescriptionTestFixture,
 
 TEST_F(TopologyDescriptionTestFixture, ShouldNotSetWireCompatibilityErrorWhenServerTypeIsUnknown) {
     const auto outgoingMinWireVersion = WireSpec::instance().outgoing.minWireVersion;
-    const auto config = SdamConfiguration(kOneServer, TopologyType::kUnknown, mongo::Seconds(10));
+    const auto config = SdamConfiguration(kOneServer, TopologyType::kUnknown);
     const auto topologyDescription = std::make_shared<TopologyDescription>(config);
     const auto serverDescriptionMaxVersion =
         ServerDescriptionBuilder().withMaxWireVersion(outgoingMinWireVersion - 1).instance();