mongodb/mongo

SERVER-49371 Revived original recursive ICE and applied to stack overflow tests

src/mongo/util/executor_test_util.h
@@ -35,9 +35,10 @@ namespace mongo {
 /**
  * An "OutOfLineExecutor" that actually runs on the same thread of execution
  * This executor is not thread-safe, and accessing it by multiple threads is prohibited.
- * Multi-threaded accesses to instances of "InlineCountingExecutor" result in undefined behavior.
+ * Multi-threaded accesses to instances of "InlineQueuedCountingExecutor" result in undefined
+ * behavior.
  */
-class InlineCountingExecutor : public OutOfLineExecutor {
+class InlineQueuedCountingExecutor : public OutOfLineExecutor {
 public:
     void schedule(Task task) override {
         // Add the task to our queue
@@ -68,13 +69,29 @@ public:
     }
 
     static auto make() {
-        return std::make_shared<InlineCountingExecutor>();
+        return std::make_shared<InlineQueuedCountingExecutor>();
     }
 
     bool inSchedule;
 
     std::deque<Task> taskQueue;
     std::atomic<uint32_t> tasksRun{0};  // NOLINT
+};
+
+class InlineRecursiveCountingExecutor final : public OutOfLineExecutor {
+public:
+    void schedule(Task task) noexcept override {
+        // Relaxed to avoid adding synchronization where there otherwise wouldn't be. That would
+        // cause a false negative from TSAN.
+        tasksRun.fetch_add(1, std::memory_order_relaxed);
+        task(Status::OK());
+    }
+
+    static auto make() {
+        return std::make_shared<InlineRecursiveCountingExecutor>();
+    }
+
+    std::atomic<int32_t> tasksRun{0};  // NOLINT
 };
 
 class RejectingExecutor final : public OutOfLineExecutor {

zhangruian/mongodb7_0

src/mongo/util/executor_test_util.h
@@ -35,9 +35,10 @@ namespace mongo {
 /**
  * An "OutOfLineExecutor" that actually runs on the same thread of execution
  * This executor is not thread-safe, and accessing it by multiple threads is prohibited.
- * Multi-threaded accesses to instances of "InlineCountingExecutor" result in undefined behavior.
+ * Multi-threaded accesses to instances of "InlineQueuedCountingExecutor" result in undefined
+ * behavior.
  */
-class InlineCountingExecutor : public OutOfLineExecutor {
+class InlineQueuedCountingExecutor : public OutOfLineExecutor {
 public:
     void schedule(Task task) override {
         // Add the task to our queue
@@ -68,12 +69,28 @@ public:
     }
 
     static auto make() {
-        return std::make_shared<InlineCountingExecutor>();
+        return std::make_shared<InlineQueuedCountingExecutor>();
     }
 
     bool inSchedule;
 
     std::deque<Task> taskQueue;
+    std::atomic<uint32_t> tasksRun{0};  // NOLINT
+};
+
+class InlineRecursiveCountingExecutor final : public OutOfLineExecutor {
+public:
+    void schedule(Task task) noexcept override {
+        // Relaxed to avoid adding synchronization where there otherwise wouldn't be. That would
+        // cause a false negative from TSAN.
+        tasksRun.fetch_add(1, std::memory_order_relaxed);
+        task(Status::OK());
+    }
+
+    static auto make() {
+        return std::make_shared<InlineRecursiveCountingExecutor>();
+    }
+
     std::atomic<uint32_t> tasksRun{0};  // NOLINT
 };
 