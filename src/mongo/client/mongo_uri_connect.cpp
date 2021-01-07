shuvalov-mdb/mongo

SERVER-53423: Make ConnectString::connect return a status instead of setting error message

src/mongo/client/mongo_uri_connect.cpp
@@ -51,27 +51,29 @@ DBClientBase* MongoURI::connect(StringData applicationName,
         }
     }
 
-    auto ret = std::unique_ptr<DBClientBase>(_connectString.connect(
-        applicationName, errmsg, socketTimeoutSecs.value_or(0.0), this, apiParameters));
-    if (!ret) {
+    auto connectionOrStatus = _connectString.connect(
+        applicationName, socketTimeoutSecs.value_or(0.0), this, apiParameters);
+    if (!connectionOrStatus.isOK()) {
+        errmsg = connectionOrStatus.getStatus().reason();
         return nullptr;
     }
 
     if (!getSetName().empty()) {
         // When performing initial topology discovery, don't bother authenticating
         // since we will be immediately restarting our connect loop to a single node.
-        return ret.release();
+        return connectionOrStatus.getValue().release();
     }
 
-    if (!ret->authenticatedDuringConnect()) {
-        auto optAuthObj =
-            makeAuthObjFromOptions(ret->getMaxWireVersion(), ret->getIsPrimarySaslMechanisms());
+    auto connection = std::move(connectionOrStatus.getValue());
+    if (!connection->authenticatedDuringConnect()) {
+        auto optAuthObj = makeAuthObjFromOptions(connection->getMaxWireVersion(),
+                                                 connection->getIsPrimarySaslMechanisms());
         if (optAuthObj) {
-            ret->auth(optAuthObj.get());
+            connection->auth(optAuthObj.get());
         }
     }
 
-    return ret.release();
+    return connection.release();
 }
 
 }  // namespace mongo

src/mongo/shell/shell_utils.cpp
@@ -131,10 +131,9 @@ namespace {
 std::unique_ptr<DBClientBase> benchRunConfigCreateConnectionImplProvider(
     const BenchRunConfig& config) {
     const ConnectionString connectionString = uassertStatusOK(ConnectionString::parse(config.host));
-    std::string errorMessage;
-    std::unique_ptr<DBClientBase> connection(connectionString.connect("BenchRun", errorMessage));
-    uassert(16158, errorMessage, connection);
-    return connection;
+    auto connectionOrStatus{connectionString.connect("BenchRun")};
+    uassert(16158, connectionOrStatus.getStatus().reason(), connectionOrStatus.isOK());
+    return std::move(connectionOrStatus.getValue());
 }
 
 auto benchRunConfigCreateConnectionImplRegistration = MONGO_WEAK_FUNCTION_REGISTRATION(

zhangruian/mongodb7_0

src/mongo/client/mongo_uri_connect.cpp
@@ -51,27 +51,29 @@ DBClientBase* MongoURI::connect(StringData applicationName,
         }
     }
 
-    auto ret = std::unique_ptr<DBClientBase>(_connectString.connect(
-        applicationName, errmsg, socketTimeoutSecs.value_or(0.0), this, apiParameters));
-    if (!ret) {
+    auto swConn = _connectString.connect(
+        applicationName, socketTimeoutSecs.value_or(0.0), this, apiParameters);
+    if (!swConn.isOK()) {
+        errmsg = swConn.getStatus().reason();
         return nullptr;
     }
 
     if (!getSetName().empty()) {
         // When performing initial topology discovery, don't bother authenticating
         // since we will be immediately restarting our connect loop to a single node.
-        return ret.release();
+        return swConn.getValue().release();
     }
 
-    if (!ret->authenticatedDuringConnect()) {
-        auto optAuthObj =
-            makeAuthObjFromOptions(ret->getMaxWireVersion(), ret->getIsPrimarySaslMechanisms());
+    auto connection = std::move(swConn.getValue());
+    if (!connection->authenticatedDuringConnect()) {
+        auto optAuthObj = makeAuthObjFromOptions(connection->getMaxWireVersion(),
+                                                 connection->getIsPrimarySaslMechanisms());
         if (optAuthObj) {
-            ret->auth(optAuthObj.get());
+            connection->auth(optAuthObj.get());
         }
     }
 
-    return ret.release();
+    return connection.release();
 }
 
 }  // namespace mongo

src/mongo/shell/shell_utils.cpp
@@ -131,10 +131,9 @@ namespace {
 std::unique_ptr<DBClientBase> benchRunConfigCreateConnectionImplProvider(
     const BenchRunConfig& config) {
     const ConnectionString connectionString = uassertStatusOK(ConnectionString::parse(config.host));
-    std::string errorMessage;
-    std::unique_ptr<DBClientBase> connection(connectionString.connect("BenchRun", errorMessage));
-    uassert(16158, errorMessage, connection);
-    return connection;
+    auto swConn{connectionString.connect("BenchRun")};
+    uassert(16158, swConn.getStatus().reason(), swConn.isOK());
+    return std::move(swConn.getValue());
 }
 
 auto benchRunConfigCreateConnectionImplRegistration = MONGO_WEAK_FUNCTION_REGISTRATION(