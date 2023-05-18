mongodb/mongo

SERVER-74806 Account for header size when computing initial size of batch write

src/mongo/db/pipeline/document_source_out.cpp
@@ -284,6 +284,13 @@ void DocumentSourceOut::finalize() {
     _timeseriesStateConsistent = true;
 }
 
+BatchedCommandRequest DocumentSourceOut::initializeBatchedWriteRequest() const {
+    // Note that our insert targets '_tempNs' (or the associated timeseries view) since we will
+    // never write to 'outputNs' directly.
+    const auto& targetNss = _timeseries ? _tempNs.getTimeseriesViewNamespace() : _tempNs;
+    return DocumentSourceWriter::makeInsertCommand(targetNss, pExpCtx->bypassDocumentValidation);
+}
+
 boost::intrusive_ptr<DocumentSource> DocumentSourceOut::create(
     NamespaceString outputNs,
     const boost::intrusive_ptr<ExpressionContext>& expCtx,

zhangruian/mongodb7_0

(cherry picked from commit 1e233afdb0def2aed7521ae087b575c95245e257)
(cherry picked from commit a1f0cfdcbcc632133a7e65665c84b9344f4d8153)

src/mongo/db/pipeline/document_source_out.cpp
@@ -183,6 +183,11 @@ void DocumentSourceOut::finalize() {
     _tempNs = {};
 }
 
+BatchedCommandRequest DocumentSourceOut::initializeBatchedWriteRequest() const {
+    // Note that our insert targets '_tempNs' since we will never write to 'outputNs' directly.
+    return DocumentSourceWriter::makeInsertCommand(_tempNs, pExpCtx->bypassDocumentValidation);
+}
+
 boost::intrusive_ptr<DocumentSource> DocumentSourceOut::create(
     NamespaceString outputNs, const boost::intrusive_ptr<ExpressionContext>& expCtx) {
 