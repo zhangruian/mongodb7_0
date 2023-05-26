mongodb/mongo

SERVER-76137 Run sharding passthroughs with a config shard in config shard variant

buildscripts/resmokelib/core/programs.py
@@ -253,7 +253,7 @@ def mongo_shell_program(logger, executable=None, connection_string=None, filenam
     test_data["undoRecorderPath"] = config.UNDO_RECORDER_PATH
 
     if "catalogShard" not in test_data and config.CATALOG_SHARD is not None:
-        test_data["catalogShard"] = config.CATALOG_SHARD
+        test_data["catalogShard"] = True
 
     # There's a periodic background thread that checks for and aborts expired transactions.
     # "transactionLifetimeLimitSeconds" specifies for how long a transaction can run before expiring

zhangruian/mongodb7_0

(cherry picked from commit 124eb7f41bfe3082cbe8067503eb02679ab20c6b)

buildscripts/resmokelib/core/programs.py
@@ -253,7 +253,7 @@ def mongo_shell_program(logger, executable=None, connection_string=None, filenam
     test_data["undoRecorderPath"] = config.UNDO_RECORDER_PATH
 
     if "configShard" not in test_data and config.CONFIG_SHARD is not None:
-        test_data["configShard"] = config.CONFIG_SHARD
+        test_data["configShard"] = True
 
     # There's a periodic background thread that checks for and aborts expired transactions.
     # "transactionLifetimeLimitSeconds" specifies for how long a transaction can run before expiring