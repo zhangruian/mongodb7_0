mongodb/mongo

SERVER-78917 Relaxing conditions in router loop

src/mongo/s/stale_shard_version_helpers.cpp
@@ -69,10 +69,11 @@ void checkErrorStatusAndMaxRetries(const Status& status,
     if (status == ErrorCodes::StaleDbVersion) {
         auto staleInfo = status.extraInfo<
             error_details::ErrorExtraInfoForImpl<ErrorCodes::StaleDbVersion>::type>();
-        invariant(staleInfo->getDb() == nss.db_forSharding(),
-                  str::stream() << "StaleDbVersion error on unexpected database. Expected "
-                                << nss.dbName().toStringForErrorMsg() << ", received "
-                                << staleInfo->getDb());
+        tassert(7891700,
+                str::stream() << "StaleDbVersion error on unexpected database. Expected "
+                              << nss.dbName().toStringForErrorMsg() << ", received "
+                              << staleInfo->getDb(),
+                staleInfo->getDb() == nss.db_forSharding());
 
         // If the database version is stale, refresh its entry in the catalog cache.
         catalogCache->onStaleDatabaseVersion(staleInfo->getDb(), staleInfo->getVersionWanted());
@@ -86,12 +87,15 @@ void checkErrorStatusAndMaxRetries(const Status& status,
         // If the cache currently considers the collection to be unsharded, this will trigger an
         // epoch refresh. If no shard is provided, then the epoch is stale and we must refresh.
         if (auto staleInfo = status.extraInfo<StaleConfigInfo>()) {
-            invariant(staleInfo->getNss() == nss,
-                      str::stream() << "StaleConfig error on unexpected namespace. Expected "
-                                    << nss.toStringForErrorMsg() << ", received "
-                                    << staleInfo->getNss().toStringForErrorMsg());
+            tassert(7891701,
+                    str::stream() << "StaleConfig error on unexpected namespace. Expected "
+                                  << nss.toStringForErrorMsg() << ", received "
+                                  << staleInfo->getNss().toStringForErrorMsg(),
+                    staleInfo->getNss() == nss ||
+                        staleInfo->getNss() == nss.makeTimeseriesBucketsNamespace());
+
             catalogCache->invalidateShardOrEntireCollectionEntryForShardedCollection(
-                nss, staleInfo->getVersionWanted(), staleInfo->getShardId());
+                staleInfo->getNss(), staleInfo->getVersionWanted(), staleInfo->getShardId());
         } else {
             catalogCache->invalidateCollectionEntry_LINEARIZABLE(nss);
         }

zhangruian/mongodb7_0

(cherry picked from commit 339eb44975cb119748b4ab8d9c4fa5b4c74be7d0)

src/mongo/s/stale_shard_version_helpers.cpp
@@ -64,10 +64,11 @@ void checkErrorStatusAndMaxRetries(const Status& status,
     if (status == ErrorCodes::StaleDbVersion) {
         auto staleInfo = status.extraInfo<
             error_details::ErrorExtraInfoForImpl<ErrorCodes::StaleDbVersion>::type>();
-        invariant(staleInfo->getDb() == nss.db(),
-                  str::stream() << "StaleDbVersion error on unexpected database. Expected "
-                                << nss.dbName().toStringForErrorMsg() << ", received "
-                                << staleInfo->getDb());
+        tassert(7891700,
+                str::stream() << "StaleDbVersion error on unexpected database. Expected "
+                              << nss.dbName().toStringForErrorMsg() << ", received "
+                              << staleInfo->getDb(),
+                staleInfo->getDb() == nss.db());
 
         // If the database version is stale, refresh its entry in the catalog cache.
         catalogCache->onStaleDatabaseVersion(staleInfo->getDb(), staleInfo->getVersionWanted());
@@ -81,11 +82,15 @@ void checkErrorStatusAndMaxRetries(const Status& status,
         // If the cache currently considers the collection to be unsharded, this will trigger an
         // epoch refresh. If no shard is provided, then the epoch is stale and we must refresh.
         if (auto staleInfo = status.extraInfo<StaleConfigInfo>()) {
-            invariant(staleInfo->getNss() == nss,
-                      str::stream() << "StaleConfig error on unexpected namespace. Expected " << nss
-                                    << ", received " << staleInfo->getNss());
+            tassert(7891701,
+                    str::stream() << "StaleConfig error on unexpected namespace. Expected "
+                                  << nss.toStringForErrorMsg() << ", received "
+                                  << staleInfo->getNss().toStringForErrorMsg(),
+                    staleInfo->getNss() == nss ||
+                        staleInfo->getNss() == nss.makeTimeseriesBucketsNamespace());
+
             catalogCache->invalidateShardOrEntireCollectionEntryForShardedCollection(
-                nss, staleInfo->getVersionWanted(), staleInfo->getShardId());
+                staleInfo->getNss(), staleInfo->getVersionWanted(), staleInfo->getShardId());
         } else {
             catalogCache->invalidateCollectionEntry_LINEARIZABLE(nss);
         }