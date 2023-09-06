mongodb/mongo

SERVER-80488 Avoid traversing routing table in balancer split chunk policy

src/mongo/db/s/balancer/balancer_chunk_selection_policy_impl.cpp
@@ -57,6 +57,30 @@ using SplitInfoVector = BalancerChunkSelectionPolicy::SplitInfoVector;
 
 namespace {
 
+StatusWith<ZoneInfo> createCollectionZoneInfo(OperationContext* opCtx,
+                                              const NamespaceString& nss,
+                                              const KeyPattern& keyPattern) {
+    ZoneInfo zoneInfo;
+    const auto swCollectionTags =
+        Grid::get(opCtx)->catalogClient()->getTagsForCollection(opCtx, nss);
+    if (!swCollectionTags.isOK()) {
+        return swCollectionTags.getStatus().withContext(
+            str::stream() << "Unable to load tags for collection " << nss);
+    }
+    const auto& collectionTags = swCollectionTags.getValue();
+
+    for (const auto& tag : collectionTags) {
+        auto status =
+            zoneInfo.addRangeToZone(ZoneRange{keyPattern.extendRangeBound(tag.getMinKey(), false),
+                                              keyPattern.extendRangeBound(tag.getMaxKey(), false),
+                                              tag.getTag()});
+        if (!status.isOK()) {
+            return status;
+        }
+    }
+    return {std::move(zoneInfo)};
+}
+
 /**
  * Does a linear pass over the information cached in the specified chunk manager and extracts chunk
  * distribution and chunk placement information which is needed by the balancer policy.
@@ -88,31 +112,13 @@ StatusWith<DistributionStatus> createCollectionDistributionStatus(
         return true;
     });
 
-    const auto swCollectionTags =
-        Grid::get(opCtx)->catalogClient()->getTagsForCollection(opCtx, nss);
-    if (!swCollectionTags.isOK()) {
-        return swCollectionTags.getStatus().withContext(
-            str::stream() << "Unable to load tags for collection " << nss);
-    }
-    const auto& collectionTags = swCollectionTags.getValue();
-
-    DistributionStatus distribution(nss, std::move(shardToChunksMap));
-
-    // Cache the collection tags
-    const auto& keyPattern = chunkMgr.getShardKeyPattern().getKeyPattern();
-
-    for (const auto& tag : collectionTags) {
-        auto status = distribution.addRangeToZone(
-            ZoneRange(keyPattern.extendRangeBound(tag.getMinKey(), false),
-                      keyPattern.extendRangeBound(tag.getMaxKey(), false),
-                      tag.getTag()));
-
-        if (!status.isOK()) {
-            return status;
-        }
+    auto swZoneInfo =
+        createCollectionZoneInfo(opCtx, nss, chunkMgr.getShardKeyPattern().getKeyPattern());
+    if (!swZoneInfo.isOK()) {
+        return swZoneInfo.getStatus();
     }
 
-    return {std::move(distribution)};
+    return {DistributionStatus{nss, std::move(shardToChunksMap), std::move(swZoneInfo.getValue())}};
 }
 
 /**
@@ -183,12 +189,12 @@ private:
  * range boundaries.
  */
 void getSplitCandidatesToEnforceTagRanges(const ChunkManager& cm,
-                                          const DistributionStatus& distribution,
+                                          const ZoneInfo& zoneInfo,
                                           SplitCandidatesBuffer* splitCandidates) {
     const auto& globalMax = cm.getShardKeyPattern().getKeyPattern().globalMax();
 
     // For each tag range, find chunks that need to be split.
-    for (const auto& tagRangeEntry : distribution.tagRanges()) {
+    for (const auto& tagRangeEntry : zoneInfo.zoneRanges()) {
         const auto& tagRange = tagRangeEntry.second;
 
         const auto chunkAtZoneMin = cm.findIntersectingChunkWithSimpleCollation(tagRange.min);
@@ -533,26 +539,26 @@ StatusWith<SplitInfoVector> BalancerChunkSelectionPolicyImpl::_getSplitCandidate
 
     const auto& cm = routingInfoStatus.getValue();
 
-    const auto collInfoStatus = createCollectionDistributionStatus(opCtx, nss, shardStats, cm);
-    if (!collInfoStatus.isOK()) {
-        return collInfoStatus.getStatus();
+    const auto swCollZoneInfo =
+        createCollectionZoneInfo(opCtx, nss, cm.getShardKeyPattern().getKeyPattern());
+    if (!swCollZoneInfo.isOK()) {
+        return swCollZoneInfo.getStatus();
     }
-
-    const DistributionStatus& distribution = collInfoStatus.getValue();
+    const auto& collZoneInfo = swCollZoneInfo.getValue();
 
     // Accumulate split points for the same chunk together
     SplitCandidatesBuffer splitCandidates(nss, cm.getVersion());
 
     if (nss == NamespaceString::kLogicalSessionsNamespace) {
-        if (!distribution.tags().empty()) {
+        if (!collZoneInfo.allZones().empty()) {
             LOGV2_WARNING(4562401,
                           "Ignoring zones for the sessions collection",
-                          "tags"_attr = distribution.tags());
+                          "tags"_attr = collZoneInfo.allZones());
         }
 
         getSplitCandidatesForSessionsCollection(opCtx, cm, &splitCandidates);
     } else {
-        getSplitCandidatesToEnforceTagRanges(cm, distribution, &splitCandidates);
+        getSplitCandidatesToEnforceTagRanges(cm, collZoneInfo, &splitCandidates);
     }
 
     return splitCandidates.done();

zhangruian/mongodb7_0

(cherry picked from commit 4fad44a858d8ee2d642566fc8872ef410f6534e4)
Remove addTagsFromCatalog

src/mongo/db/s/balancer/balancer_chunk_selection_policy_impl.cpp
@@ -91,17 +91,13 @@ StatusWith<DistributionStatus> createCollectionDistributionStatus(
         return true;
     });
 
-    const auto& keyPattern = chunkMgr.getShardKeyPattern().getKeyPattern();
-
-    // Cache the collection zones
-    auto swZoneInfo = ZoneInfo::getZonesForCollection(opCtx, nss, keyPattern);
+    auto swZoneInfo =
+        createCollectionZoneInfo(opCtx, nss, chunkMgr.getShardKeyPattern().getKeyPattern());
     if (!swZoneInfo.isOK()) {
         return swZoneInfo.getStatus();
     }
 
-    DistributionStatus distribution(nss, std::move(shardToChunksMap), swZoneInfo.getValue());
-
-    return {std::move(distribution)};
+    return {DistributionStatus{nss, std::move(shardToChunksMap), std::move(swZoneInfo.getValue())}};
 }
 
 stdx::unordered_map<NamespaceString, CollectionDataSizeInfoForBalancing>
@@ -250,12 +246,12 @@ private:
  * range boundaries.
  */
 void getSplitCandidatesToEnforceZoneRanges(const ChunkManager& cm,
-                                           const DistributionStatus& distribution,
+                                           const ZoneInfo& zoneInfo,
                                            SplitCandidatesBuffer* splitCandidates) {
     const auto& globalMax = cm.getShardKeyPattern().getKeyPattern().globalMax();
 
     // For each zone range, find chunks that need to be split.
-    for (const auto& zoneRangeEntry : distribution.zoneRanges()) {
+    for (const auto& zoneRangeEntry : zoneInfo.zoneRanges()) {
         const auto& zoneRange = zoneRangeEntry.second;
 
         const auto chunkAtZoneMin = cm.findIntersectingChunkWithSimpleCollation(zoneRange.min);
@@ -645,23 +641,23 @@ StatusWith<SplitInfoVector> BalancerChunkSelectionPolicyImpl::_getSplitCandidate
 
     const auto& [cm, _] = routingInfoStatus.getValue();
 
-    const auto collInfoStatus = createCollectionDistributionStatus(opCtx, nss, shardStats, cm);
-    if (!collInfoStatus.isOK()) {
-        return collInfoStatus.getStatus();
+    const auto swCollZoneInfo =
+        createCollectionZoneInfo(opCtx, nss, cm.getShardKeyPattern().getKeyPattern());
+    if (!swCollZoneInfo.isOK()) {
+        return swCollZoneInfo.getStatus();
     }
-
-    const DistributionStatus& distribution = collInfoStatus.getValue();
+    const auto& collZoneInfo = swCollZoneInfo.getValue();
 
     // Accumulate split points for the same chunk together
     SplitCandidatesBuffer splitCandidates(nss, cm.getVersion());
 
-    if (nss == NamespaceString::kLogicalSessionsNamespace && !distribution.zones().empty()) {
+    if (nss == NamespaceString::kLogicalSessionsNamespace && !collZoneInfo.allZones().empty()) {
         LOGV2_WARNING(4562401,
                       "Ignoring zones for the internal sessions collection.",
                       "nss"_attr = NamespaceString::kLogicalSessionsNamespace,
-                      "zones"_attr = distribution.zones());
+                      "zones"_attr = collZoneInfo.allZones());
     } else {
-        getSplitCandidatesToEnforceZoneRanges(cm, distribution, &splitCandidates);
+        getSplitCandidatesToEnforceZoneRanges(cm, collZoneInfo, &splitCandidates);
     }
 
     return splitCandidates.done();