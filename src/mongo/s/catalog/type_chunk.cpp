mongodb/mongo

SERVER-77506 Expose the maxValidAfter timestamp alongside the shardVersion

src/mongo/s/catalog/type_chunk.cpp
@@ -641,8 +641,9 @@ Status ChunkType::validate() const {
     if (!_history.empty()) {
         if (_history.front().getShard() != *_shard) {
             return {ErrorCodes::BadValue,
-                    str::stream() << "History contains an invalid shard "
-                                  << _history.front().getShard()};
+                    str::stream() << "Latest entry of chunk history refer to shard "
+                                  << _history.front().getShard()
+                                  << " that does not match the current shard " << *_shard};
         }
         if (_onCurrentShardSince.has_value() &&
             _history.front().getValidAfter() != *_onCurrentShardSince) {

src/mongo/s/routing_table_history_test.cpp
@@ -75,6 +75,7 @@ namespace mongo {
 using chunks_test_util::assertEqualChunkInfo;
 using chunks_test_util::calculateCollVersion;
 using chunks_test_util::calculateIntermediateShardKey;
+using chunks_test_util::calculateShardsMaxValidAfter;
 using chunks_test_util::calculateShardVersions;
 using chunks_test_util::genChunkVector;
 using chunks_test_util::genRandomChunkVector;
@@ -278,6 +279,17 @@ TEST_F(RoutingTableHistoryTest, RandomCreateBasic) {
     std::set<ShardId> shardIds;
     rt.getAllShardIds(&shardIds);
     ASSERT(expectedShardIds == shardIds);
+
+    // Validate all shard maxValidAfter
+    const auto expectedShardsMaxValidAfter = calculateShardsMaxValidAfter(chunks);
+    const auto expectedMaxValidAfter = [&](const ShardId& shard) {
+        auto it = expectedShardsMaxValidAfter.find(shard);
+        return it != expectedShardsMaxValidAfter.end() ? it->second : Timestamp{0, 0};
+    };
+    for (const auto& [shardId, _] : expectedShardVersions) {
+        ASSERT_EQ(rt.getMaxValidAfter(shardId), expectedMaxValidAfter(shardId));
+    }
+    ASSERT_EQ(rt.getMaxValidAfter(ShardId{"shard-without-chunks"}), (Timestamp{0, 0}));
 }
 
 /*
@@ -296,6 +308,7 @@ TEST_F(RoutingTableHistoryTest, RandomCreateWithMissingChunkFail) {
         // TODO SERVER-77090: stop forcing chunks on different shards
         for (size_t i = 0; i < chunks.size(); i++) {
             chunks[i].setShard(getShardId(i));
+            chunks[i].setHistory({});
         }
     }
 
@@ -321,6 +334,7 @@ TEST_F(RoutingTableHistoryTest, RandomCreateWithChunkGapFail) {
         // TODO SERVER-77090: stop forcing chunks on different shards
         for (size_t i = 0; i < chunks.size(); i++) {
             chunks[i].setShard(getShardId(i));
+            chunks[i].setHistory({});
         }
     }
 
@@ -351,6 +365,7 @@ TEST_F(RoutingTableHistoryTest, RandomUpdateWithChunkGapFail) {
         // TODO SERVER-77090: stop forcing chunks on different shards
         for (size_t i = 0; i < chunks.size(); i++) {
             chunks[i].setShard(getShardId(i));
+            chunks[i].setHistory({});
         }
     }
 
@@ -393,6 +408,7 @@ TEST_F(RoutingTableHistoryTest, RandomCreateWithChunkOverlapFail) {
         // TODO SERVER-77090: stop forcing chunks on different shards
         for (size_t i = 0; i < chunks.size(); i++) {
             chunks[i].setShard(getShardId(i));
+            chunks[i].setHistory({});
         }
     }
 
@@ -437,6 +453,7 @@ TEST_F(RoutingTableHistoryTest, RandomUpdateWithChunkOverlapFail) {
         // TODO SERVER-77090: stop forcing chunks on different shards
         for (size_t i = 0; i < chunks.size(); i++) {
             chunks[i].setShard(getShardId(i));
+            chunks[i].setHistory({});
         }
     }
     // Create a new routing table from the randomly generated chunks
@@ -612,6 +629,18 @@ TEST_F(RoutingTableHistoryTest, RandomUpdate) {
     std::set<ShardId> shardIds;
     rt.getAllShardIds(&shardIds);
     ASSERT(expectedShardIds == shardIds);
+
+    // Validate all shard maxValidAfter
+    const auto expectedShardsMaxValidAfter = calculateShardsMaxValidAfter(chunks);
+    const auto expectedMaxValidAfter = [&](const ShardId& shard) {
+        auto it = expectedShardsMaxValidAfter.find(shard);
+        return it != expectedShardsMaxValidAfter.end() ? it->second : Timestamp{0, 0};
+    };
+    for (const auto& [shardId, _] : expectedShardVersions) {
+        ASSERT_EQ(rt.getMaxValidAfter(shardId), expectedMaxValidAfter(shardId))
+            << "For shardid " << shardId;
+    }
+    ASSERT_EQ(rt.getMaxValidAfter(ShardId{"shard-without-chunks"}), (Timestamp{0, 0}));
 }
 
 TEST_F(RoutingTableHistoryTest, TestSplits) {

zhangruian/mongodb7_0

(cherry picked from commit da006e938e12bd81f1fc2a6a5a86b7b177dfe66f)

src/mongo/s/catalog/type_chunk.cpp
@@ -641,8 +641,9 @@ Status ChunkType::validate() const {
     if (!_history.empty()) {
         if (_history.front().getShard() != *_shard) {
             return {ErrorCodes::BadValue,
-                    str::stream() << "History contains an invalid shard "
-                                  << _history.front().getShard()};
+                    str::stream() << "Latest entry of chunk history refers to shard "
+                                  << _history.front().getShard()
+                                  << " that does not match the current shard " << *_shard};
         }
         if (_onCurrentShardSince.has_value() &&
             _history.front().getValidAfter() != *_onCurrentShardSince) {

src/mongo/s/routing_table_history_test.cpp
@@ -47,6 +47,7 @@ namespace mongo {
 using chunks_test_util::assertEqualChunkInfo;
 using chunks_test_util::calculateCollVersion;
 using chunks_test_util::calculateIntermediateShardKey;
+using chunks_test_util::calculateShardsMaxValidAfter;
 using chunks_test_util::calculateShardVersions;
 using chunks_test_util::genChunkVector;
 using chunks_test_util::genRandomChunkVector;
@@ -251,6 +252,17 @@ TEST_F(RoutingTableHistoryTest, RandomCreateBasic) {
     std::set<ShardId> shardIds;
     rt.getAllShardIds(&shardIds);
     ASSERT(expectedShardIds == shardIds);
+
+    // Validate all shard maxValidAfter
+    const auto expectedShardsMaxValidAfter = calculateShardsMaxValidAfter(chunks);
+    const auto expectedMaxValidAfter = [&](const ShardId& shard) {
+        auto it = expectedShardsMaxValidAfter.find(shard);
+        return it != expectedShardsMaxValidAfter.end() ? it->second : Timestamp{0, 0};
+    };
+    for (const auto& [shardId, _] : expectedShardVersions) {
+        ASSERT_GTE(rt.getMaxValidAfter(shardId), expectedMaxValidAfter(shardId));
+    }
+    ASSERT_EQ(rt.getMaxValidAfter(ShardId{"shard-without-chunks"}), (Timestamp{0, 0}));
 }
 
 /*
@@ -539,6 +551,18 @@ TEST_F(RoutingTableHistoryTest, RandomUpdate) {
     std::set<ShardId> shardIds;
     rt.getAllShardIds(&shardIds);
     ASSERT(expectedShardIds == shardIds);
+
+    // Validate all shard maxValidAfter
+    const auto expectedShardsMaxValidAfter = calculateShardsMaxValidAfter(chunks);
+    const auto expectedMaxValidAfter = [&](const ShardId& shard) {
+        auto it = expectedShardsMaxValidAfter.find(shard);
+        return it != expectedShardsMaxValidAfter.end() ? it->second : Timestamp{0, 0};
+    };
+    for (const auto& [shardId, _] : expectedShardVersions) {
+        ASSERT_GTE(rt.getMaxValidAfter(shardId), expectedMaxValidAfter(shardId))
+            << "For shardid " << shardId;
+    }
+    ASSERT_EQ(rt.getMaxValidAfter(ShardId{"shard-without-chunks"}), (Timestamp{0, 0}));
 }
 
 TEST_F(RoutingTableHistoryTest, AllowMigrationFlag) {