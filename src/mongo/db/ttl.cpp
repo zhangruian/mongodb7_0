mongodb/mongo

SERVER-79864 Disable TTL on time-series colls requiring extended range

src/mongo/db/ttl.cpp
@@ -647,6 +647,10 @@ bool TTLMonitor::_doTTLIndexDelete(OperationContext* opCtx,
             return false;
         }
 
+        if (collectionPtr->getRequiresTimeseriesExtendedRangeSupport()) {
+            return false;
+        }
+
         ResourceConsumption::ScopedMetricsCollector scopedMetrics(opCtx, nss->dbName());
 
         if (info.isClustered()) {

zhangruian/mongodb7_0

(cherry picked from commit aa87caf6b24095a6e907d76c9b27fc44a63ed7d3)

src/mongo/db/ttl.cpp
@@ -589,6 +589,10 @@ bool TTLMonitor::_doTTLIndexDelete(OperationContext* opCtx,
             return false;
         }
 
+        if (coll->getRequiresTimeseriesExtendedRangeSupport()) {
+            return false;
+        }
+
         ResourceConsumption::ScopedMetricsCollector scopedMetrics(opCtx, nss->db().toString());
 
         const auto& collection = coll.getCollection();