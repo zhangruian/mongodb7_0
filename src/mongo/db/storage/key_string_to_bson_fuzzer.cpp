mongodb/mongo

SERVER-82730 Convert invariants when decoding keystring RecordId to assertions

src/mongo/db/storage/key_string_to_bson_fuzzer.cpp
@@ -121,5 +121,17 @@ extern "C" int LLVMFuzzerTestOneInput(const char* Data, size_t Size) {
         // We need to catch exceptions caused by invalid inputs
     }
 
+    try {
+        mongo::key_string::decodeRecordIdLongAtEnd(&Data[2 + len], Size - (2 + len));
+    } catch (const mongo::AssertionException&) {
+        // We need to catch exceptions caused by invalid inputs
+    }
+
+    try {
+        mongo::key_string::decodeRecordIdStrAtEnd(&Data[2 + len], Size - (2 + len));
+    } catch (const mongo::AssertionException&) {
+        // We need to catch exceptions caused by invalid inputs
+    }
+
     return 0;
 }

zhangruian/mongodb7_0

(cherry picked from commit bf607bda808e7a9ab70ed5d5260161d548c035ce)

src/mongo/db/storage/key_string_to_bson_fuzzer.cpp
@@ -121,5 +121,17 @@ extern "C" int LLVMFuzzerTestOneInput(const char* Data, size_t Size) {
         // We need to catch exceptions caused by invalid inputs
     }
 
+    try {
+        mongo::KeyString::decodeRecordIdLongAtEnd(&Data[2 + len], Size - (2 + len));
+    } catch (const mongo::AssertionException&) {
+        // We need to catch exceptions caused by invalid inputs
+    }
+
+    try {
+        mongo::KeyString::decodeRecordIdStrAtEnd(&Data[2 + len], Size - (2 + len));
+    } catch (const mongo::AssertionException&) {
+        // We need to catch exceptions caused by invalid inputs
+    }
+
     return 0;
 }