mongodb/mongo

SERVER-76560 Honor `expireAfterSeconds` for time-series pre-2018

src/mongo/db/ttl.cpp
@@ -835,7 +835,7 @@ bool TTLMonitor::_deleteExpiredWithCollscan(OperationContext* opCtx,
 
     LOGV2_DEBUG(5400704, 1, "running TTL job for clustered collection", logAttrs(collection.nss()));
 
-    const auto startId = makeCollScanStartBound(collectionPtr, Date_t::min());
+    const auto startId = makeCollScanStartBound(collectionPtr, Date_t{});
 
     const auto expirationDate = safeExpirationDate(opCtx, collectionPtr, *expireAfterSeconds);
     const auto endId = makeCollScanEndBound(collectionPtr, expirationDate);

zhangruian/mongodb7_0

(cherry picked from commit 21b7b619eeb6b23bee47ff493b708255fdf6d95e)

src/mongo/db/ttl.cpp
@@ -780,7 +780,7 @@ bool TTLMonitor::_deleteExpiredWithCollscan(OperationContext* opCtx,
 
     LOGV2_DEBUG(5400704, 1, "running TTL job for clustered collection", logAttrs(collection->ns()));
 
-    const auto startId = makeCollScanStartBound(collection, Date_t::min());
+    const auto startId = makeCollScanStartBound(collection, Date_t{});
 
     const auto expirationDate = safeExpirationDate(opCtx, collection, *expireAfterSeconds);
     const auto endId = makeCollScanEndBound(collection, expirationDate);