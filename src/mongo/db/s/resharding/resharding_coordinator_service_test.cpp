pmeredit/mongo

SERVER-88651 Ensure resharding metrics object still exists when reporting currentOp info (#20924)

src/mongo/db/s/resharding/resharding_coordinator_service_test.cpp
@@ -913,6 +913,54 @@ TEST_F(ReshardingCoordinatorServiceTest, StepDownStepUpEachTransition) {
     }
 }
 
+TEST_F(ReshardingCoordinatorServiceTest, ReportForCurrentOpAfterCompletion) {
+    auto pauseAfterInsertCoordinatorDoc =
+        globalFailPointRegistry().find("pauseAfterInsertCoordinatorDoc");
+    auto timesEnteredFailPoint = pauseAfterInsertCoordinatorDoc->setMode(FailPoint::alwaysOn);
+
+    auto coordinator = initializeAndGetCoordinator();
+    auto instanceId =
+        BSON(ReshardingCoordinatorDocument::kReshardingUUIDFieldName << _reshardingUUID);
+
+    // Wait until we know we've inserted the coordinator doc, but before the coordinator contacts
+    // any participants so that the coordinator does not have to "wait" for participants to abort
+    // before finishing aborting itself
+    pauseAfterInsertCoordinatorDoc->waitForTimesEntered(timesEnteredFailPoint + 1);
+
+    // Force a failover, and wait for the state machine to fulfill the completion promise. At this
+    // point, the resharding metrics will have been unregistered from the cumulative metrics.
+    stepDown(operationContext());
+    pauseAfterInsertCoordinatorDoc->setMode(FailPoint::off);
+    ASSERT_EQ(coordinator->getCompletionFuture().getNoThrow(), ErrorCodes::CallbackCanceled);
+
+    // Now call step up. The old coordinator object has not yet been destroyed because we still hold
+    // a shared pointer to it ('coordinator') - this can happen in production after a failover if a
+    // state machine is slow to clean up. Wait for the coordinator to have started, but again don't
+    // let it move to a state where it contacts participants.
+    auto pauseBeforeCTHolderInitialization =
+        globalFailPointRegistry().find("pauseBeforeCTHolderInitialization");
+    timesEnteredFailPoint = pauseBeforeCTHolderInitialization->setMode(FailPoint::alwaysOn);
+
+    stepUp(operationContext());
+    pauseBeforeCTHolderInitialization->waitForTimesEntered(timesEnteredFailPoint + 1);
+
+    // Assert that the old coordinator object will return a currentOp report, because the resharding
+    // metrics still exist on the coordinator object itelf.
+    ASSERT(coordinator->reportForCurrentOp(
+        MongoProcessInterface::CurrentOpConnectionsMode::kExcludeIdle,
+        MongoProcessInterface::CurrentOpSessionsMode::kIncludeIdle));
+
+    // Ensure a new coordinator can start and register resharding metrics, despite the "zombie"
+    // state machine still existing.
+    auto newCoordinator = getCoordinator(operationContext(), instanceId);
+    ASSERT_NE(coordinator, newCoordinator);
+
+    // No need to finish the resharding op, so we just cancel the op.
+    newCoordinator->abort(true /* skipQuiescePeriod */);
+    pauseBeforeCTHolderInitialization->setMode(FailPoint::off);
+    newCoordinator->getCompletionFuture().wait();
+}
+
 TEST_F(ReshardingCoordinatorServiceTest, ReshardingCoordinatorFailsIfMigrationNotAllowed) {
     auto doc = insertStateAndCatalogEntries(CoordinatorStateEnum::kUnused, _originalEpoch);
     auto opCtx = operationContext();

zhangruian/mongodb7_0

(cherry picked from commit d5f8c31907c622f2ab8b864ec1b21cd2523ecd1c)
GitOrigin-RevId: 3cec112db40d3a905253206b55900c7ca5ef5fd6

src/mongo/db/s/resharding/resharding_coordinator_service_test.cpp
@@ -885,6 +885,54 @@ TEST_F(ReshardingCoordinatorServiceTest, StepDownStepUpEachTransition) {
     }
 }
 
+TEST_F(ReshardingCoordinatorServiceTest, ReportForCurrentOpAfterCompletion) {
+    auto pauseBeforeInsertCoordinatorDoc =
+        globalFailPointRegistry().find("pauseBeforeInsertCoordinatorDoc");
+    auto timesEnteredFailPoint = pauseBeforeInsertCoordinatorDoc->setMode(FailPoint::alwaysOn);
+
+    auto coordinator = initializeAndGetCoordinator();
+    auto instanceId =
+        BSON(ReshardingCoordinatorDocument::kReshardingUUIDFieldName << _reshardingUUID);
+
+    // Wait until we know we've inserted the coordinator doc, but before the coordinator contacts
+    // any participants so that the coordinator does not have to "wait" for participants to abort
+    // before finishing aborting itself
+    pauseBeforeInsertCoordinatorDoc->waitForTimesEntered(timesEnteredFailPoint + 1);
+
+    // Force a failover, and wait for the state machine to fulfill the completion promise. At this
+    // point, the resharding metrics will have been unregistered from the cumulative metrics.
+    stepDown(operationContext());
+    pauseBeforeInsertCoordinatorDoc->setMode(FailPoint::off);
+    ASSERT_EQ(coordinator->getCompletionFuture().getNoThrow(), ErrorCodes::CallbackCanceled);
+
+    // Now call step up. The old coordinator object has not yet been destroyed because we still hold
+    // a shared pointer to it ('coordinator') - this can happen in production after a failover if a
+    // state machine is slow to clean up. Wait for the coordinator to have started, but again don't
+    // let it move to a state where it contacts participants.
+    auto pauseBeforeCTHolderInitialization =
+        globalFailPointRegistry().find("pauseBeforeCTHolderInitialization");
+    timesEnteredFailPoint = pauseBeforeCTHolderInitialization->setMode(FailPoint::alwaysOn);
+
+    stepUp(operationContext());
+    pauseBeforeCTHolderInitialization->waitForTimesEntered(timesEnteredFailPoint + 1);
+
+    // Assert that the old coordinator object will return a currentOp report, because the resharding
+    // metrics still exist on the coordinator object itelf.
+    ASSERT(coordinator->reportForCurrentOp(
+        MongoProcessInterface::CurrentOpConnectionsMode::kExcludeIdle,
+        MongoProcessInterface::CurrentOpSessionsMode::kIncludeIdle));
+
+    // Ensure a new coordinator can start and register resharding metrics, despite the "zombie"
+    // state machine still existing.
+    auto newCoordinator = getCoordinator(operationContext(), instanceId);
+    ASSERT_NE(coordinator, newCoordinator);
+
+    // No need to finish the resharding op, so we just cancel the op.
+    newCoordinator->abort();
+    pauseBeforeCTHolderInitialization->setMode(FailPoint::off);
+    newCoordinator->getCompletionFuture().wait();
+}
+
 TEST_F(ReshardingCoordinatorServiceTest, ReshardingCoordinatorFailsIfMigrationNotAllowed) {
     auto doc = insertStateAndCatalogEntries(CoordinatorStateEnum::kUnused, _originalEpoch);
     auto opCtx = operationContext();